77ce1c5130c2f0a1596b8dcb6c73370483e0fd06
stefanbabukov <stefanbabukov98@gmail.com>
2023-04-11 17:19:17 +0100

    fix

--- create_architecture.py
+++ create_architecture.py
@@ -1,9 +1,49 @@
 import boto3
-
-
+import json
 # Creating EC2 instance
 ec2 = boto3.resource('ec2')
 
+# Creating IAM client
+iam = boto3.client('iam')
+
+# IAM role name
+role_name = 'ec2_s3_access'
+bucket_name = '1919196-s3'
+# Check if IAM role already exists
+try:
+    role = iam.get_role(RoleName=role_name)
+    role_arn = role['Role']['Arn']
+    print(f"IAM role {role_name} already exists")
+except iam.exceptions.NoSuchEntityException:
+    # Create IAM role if it does not exist
+    assume_role_policy_document = {
+        'Version': '2012-10-17',
+        'Statement': [
+            {
+                'Effect': 'Allow',
+                'Action': 's3:PutObject',
+                'Resource': 'arn:aws:s3::bucket_name*'
+            }
+        ]
+    }
+
+    create_role_response = iam.create_role(
+        RoleName=role_name,
+        AssumeRolePolicyDocument=json.dumps(assume_role_policy_document)
+    )
+
+    role_arn = create_role_response['Role']['Arn']
+    print(f"IAM role {role_name} created")
+
+    # Attach S3 access policy to IAM role
+    policy_arn = 'arn:aws:iam::aws:policy/AmazonS3FullAccess'
+    iam.attach_role_policy(
+        RoleName=role_name,
+        PolicyArn=policy_arn
+    )
+    print(f"Attached S3 access policy {policy_arn} to IAM role {role_name}")
+
+# Creating EC2 instance
 instances = ec2.create_instances(
         ImageId="ami-06e46074ae430fba6",
         MinCount=1,
@@ -20,13 +60,19 @@ instances = ec2.create_instances(
                 },
             ]
         },
-    ]
-    )
+    ],
+    IamInstanceProfile={
+        'Arn': role_arn
+    }
+)
+
+print(f"EC2 instance created with IAM role {role_name}")
+
 
 # Creating S3 bucket
 
 s3 = boto3.resource('s3')
-s3.create_bucket(Bucket='1919196-s3')
+s3.create_bucket(Bucket=bucket_name)
 
 # Creating SQS queue and DynamoDB table
 cloudformation = boto3.client('cloudformation')
